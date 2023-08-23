57b84763236f95914df38f24dd58ed004f781afa
eleanort4 <142965919+eleanort4@users.noreply.github.com>
2023-08-23 17:23:50 +1200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+var Box ;
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  Box = createSprite (200,200,50,50)
+}
+
+
+
+
+
+
+function draw() 
+{
+  background(30);
+  drawSprites ();
+
+}
+
+
+
+
